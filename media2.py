b8bb69833219411555f54e7154f57e33f93b7713
filipamendonca80 <filipacastro80@gmail.com>
2026-06-26 22:13:56 +0100

    Add script to calculate the average of three numbers

---
+++ media2.py
@@ -0,0 +1,14 @@
+# Início
+
+# Ler os três números
+num1 = int(input("Introduza n1: "))
+num2 = int(input("Introduza n2: "))
+num3 = int(input("Introduza n3: "))
+
+# Calcular a média
+media = (num1 + num2 + num3) / 3
+
+# Escrever o resultado
+print("A média é:", media)
+
+# Fim
